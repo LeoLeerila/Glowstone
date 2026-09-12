4a867965b84d5b8a20e49cbc63fbfc6110e3d34e
Leo Leerilä <Leo.Leerila@metropolia.fi>
2026-09-12 17:33:18 +0300

    add mariadb docker-compose file

--- glowstone/src/main/java/glowstone/Main.java
+++ glowstone/src/main/java/glowstone/Main.java
@@ -1,7 +1,16 @@
 package glowstone;
 
+import glowstone.model.DB;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            DB.startConnection();
+            DB.getFromDB("note", 1);
+            DB.endConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- glowstone/src/main/java/glowstone/model/DBConnector.java
+++ glowstone/src/main/java/glowstone/model/DBConnector.java
@@ -1,17 +1,26 @@
-package glowstone.Model;
+package glowstone.model;
 
+import org.mariadb.jdbc.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
     private static Connection conn = null;
-    private static String connectionUrl = "jdbc:mariadb://localhost/";
+    private static String connectionUrl = "jdbc:mariadb://localhost:3306/glowstone_test";
     private static String connectionUser = "root";
-    private static String connectionPassword = "Example";
+    private static String connectionPassword = "example";
 
-    private static void connect() throws SQLException {
-        conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+    private static void connect() {
+        try {
+            if (!DriverManager.getDrivers().hasMoreElements()) {
+                System.out.println("you done fucked up now");
+            }
+            conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     public static Connection getInstance() throws SQLException {
