508b7fa68cf49aa60826748b950b03c43a5c5c1f
Leo Leerilä <Leo.Leerila@metropolia.fi>
2026-09-14 18:36:19 +0300

    add DB create functions

--- glowstone/src/main/java/glowstone/Main.java
+++ glowstone/src/main/java/glowstone/Main.java
@@ -1,5 +1,9 @@
 package glowstone;
 
+import java.sql.ResultSet;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import glowstone.model.DB;
 
 public class Main {
@@ -7,9 +11,31 @@ public class Main {
         System.out.println("Hello world!");
         try {
             DB.startConnection();
+
+            DB.insertTabToDB("java created tab", 0);
+            ResultSet rs = DB.readFromDB("NOTE_TAB", 1);
+            while (rs.next()) {
+                System.out.println("id " + rs.getInt("id") + ", name " + rs.getString("name"));
+            }
+
+            DB.insertGroupToDB("java created group", 1, 0);
+            rs = DB.readFromDB("NOTE_GROUP", 1);
+            while (rs.next()) {
+                System.out.println("id " + rs.getInt("id") + ", name " + rs.getString("name"));
+            }
+
+            //INSERT INTO `NOTE` (`content`, `name`, `group_id`, `thumbnail_id`)
+            //                     NULL     NOTNULL   NOT NULL     NULL
+            DB.insertNoteToDB("java created note content", "java created note name", 1, 0);
+            rs = DB.readNoteByGroup(1);
+            while (rs.next()) {
+                System.out.println("id " + rs.getInt("id") + ", content " + rs.getString("content") + ", name " + rs.getString("name") + ", group_id " + rs.getInt("group_id") + ", thumbnail_id " + rs.getInt("thumbnail_id"));
+            }
+
             DB.readFromDB("NOTE", 1);
             DB.readNoteByGroup(1);
             DB.readGroupByTab(1);
+
             DB.endConnection();
         } catch (Exception e) {
             e.printStackTrace();
--- glowstone/src/main/java/glowstone/model/DB.java
+++ glowstone/src/main/java/glowstone/model/DB.java
@@ -2,6 +2,9 @@ package glowstone.model;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Dictionary;
+import java.util.Enumeration;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -15,6 +18,67 @@ public class DB {
     public static void endConnection() throws SQLException {
         DBConnector.closeConnection();
         db = null;
+    }
+
+    //create
+    //INSERT INTO `NOTE` (`content`, `name`, `group_id`, `thumbnail_id`)
+    public static int insertNoteToDB(String content, String name, int group_id, int thumbnail_id){
+        int result = 0;
+        try {
+            PreparedStatement statement = db.prepareStatement("INSERT INTO `NOTE` (`content`, `name`, `group_id`, `thumbnail_id`) VALUES (?, ?, ?, ?)");
+            statement.setString(1, content);
+            statement.setString(2, name);
+            statement.setInt(3, group_id);
+            if (thumbnail_id == 0) {
+                statement.setObject(4, null, Types.INTEGER);
+            } else {
+                statement.setInt(4, thumbnail_id);
+            }
+            result = statement.executeUpdate();
+            System.out.println("inserted into NOTE");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //INSERT INTO `NOTE_GROUP` (`name`, `tab_id`, `thumbnail_id`)
+    public static int insertGroupToDB(String name, int tab_id, int thumbnail_id){
+        int result = 0;
+        try {
+            PreparedStatement statement = db.prepareStatement("INSERT INTO `NOTE_GROUP` (`name`, `tab_id`, `thumbnail_id`) VALUES (?, ?, ?)");
+            statement.setString(1, name);
+            statement.setInt(2, tab_id);
+            if (thumbnail_id == 0) {
+                statement.setObject(3, null, Types.INTEGER);
+            } else {
+                statement.setInt(3, thumbnail_id);
+            }
+            result = statement.executeUpdate();
+            System.out.println("inserted into NOTE_GROUP");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //INSERT INTO `NOTE_TAB` (`name`, `thumbnail_id`)
+    public static int insertTabToDB(String name, int thumbnail_id){
+        int result = 0;
+        try {
+            PreparedStatement statement = db.prepareStatement("INSERT INTO `NOTE_TAB` (`name`, `thumbnail_id`) VALUES (?, ?)");
+            statement.setString(1, name);
+            if (thumbnail_id == 0) {
+                statement.setObject(2, null, Types.INTEGER);
+            } else {
+                statement.setInt(2, thumbnail_id);
+            }
+            result = statement.executeUpdate();
+            System.out.println("inserted into NOTE_TAB");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     //read
