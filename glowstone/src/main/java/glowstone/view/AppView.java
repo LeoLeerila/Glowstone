e86b545205240a18c4b3bc72cd8f19e26d5168f3
Remi M <remimaskulin@gmail.com>
2026-09-14 22:08:27 +0300

    Added settings button popup, fixed an issue with elements not scaling with the window

--- glowstone/src/main/java/glowstone/view/AppView.java
+++ glowstone/src/main/java/glowstone/view/AppView.java
@@ -1,16 +1,21 @@
 package glowstone.view;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 public class AppView extends Application {
+    @FXML
+    private BorderPane main_area;
     @Override
     public void start(Stage stage) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/appview.FXML"));
         Parent root = fxmlLoader.load();
-        stage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.setTitle("Glowstone");
         stage.show();
     }
